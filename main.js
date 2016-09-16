db7291b4a6f6180f0952b63b0b3c8c003174ac89
Casey Schneebeck <cschneebeck@tnc.org>
2016-09-16 11:46:24 -0500

    dropdown

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, lang, obj,
 			}	
 			// Define object to access global variables from JSON object. Only add variables to varObject.json that are needed by Save and Share. 
 			this.obj = dojo.eval("[" + obj + "]")[0];	
-			this.url = "http://services.coastalresilience.org:6080/arcgis/rest/services/North_Carolina/NC_CRS/MapServer";
+			this.url = "http://dev.services2.coastalresilience.org:6080/arcgis/rest/services/North_Carolina/NC_CRS/MapServer";
 			this.layerDefs = [];
 		},
 		// Called after initialize at plugin startup (why all the tests for undefined). Also called after deactivate when user closes app by clicking X. 
