1a99cf80a22642bfe3fb6ed34fcb696af1b67644
Casey Schneebeck <cschneebeck@tnc.org>
2017-06-08 10:22:08 -0600

    make opening div show up in safari

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-// Pull in your favorite version of jquery 
-require({ 
-	packages: [{ name: "jquery", location: "http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/", main: "jquery.min" }] 
-});
 // Bring in dojo and javascript api classes as well as varObject.json, js files, and content.html
 define([
 	"dojo/_base/declare", "framework/PluginBase", "dijit/layout/ContentPane", "dojo/dom", "dojo/dom-style", "dojo/dom-geometry", "dojo/text!./obj.json", 
-	"dojo/text!./html/content.html", './js/jquery-ui-1.11.2/jquery-ui', './js/esriapi', './js/clicks', './js/future', './js/parcels', 'dojo/_base/lang'	
+	"dojo/text!./html/content.html", './js/esriapi', './js/clicks', './js/future', './js/parcels', 'dojo/_base/lang'	
 ],
-function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, obj, content, ui, esriapi, clicks, future, parcels, lang ) {
+function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, obj, content, esriapi, clicks, future, parcels, lang ) {
 	return declare(PluginBase, {
 		// The height and width are set here when an infographic is defined. When the user click Continue it rebuilds the app window with whatever you put in.
 		toolbarName: "Community Rating System", showServiceLayersInLegend: true, allowIdentifyWhenActive: false, rendered: false, resizable: false,
@@ -80,8 +76,6 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, obj, conte
 		},	
 		// Called by activate and builds the plugins elements and functions
 		render: function() {
-			//this.oid = -1;
-			//$('.basemap-selector').trigger('change', 3);
 			this.mapScale  = this.map.getScale();
 			// BRING IN OTHER JS FILES
 			this.esriapi = new esriapi();
@@ -100,13 +94,9 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, obj, conte
 				});
 			})
 			// Define Content Pane as HTML parent		
-			this.appDiv = new ContentPane({style:'padding:0; flex:1; display:flex; flex-direction:column;}'});
+			this.appDiv = new ContentPane({style:'padding:0; min-height:500px;}'});
 			this.id = this.appDiv.id
-			dom.byId(this.container).appendChild(this.appDiv.domNode);	
-			$('#' + this.id).parent().addClass('flexColumn')
-			if (this.obj.stateSet == "no"){
-				$('#' + this.id).parent().parent().css('display', 'flex')
-			}		
+			dom.byId(this.container).appendChild(this.appDiv.domNode);		
 			// Get html from content.html, prepend appDiv.id to html element id's, and add to appDiv
 			var idUpdate0 = content.replace(/for="/g, 'for="' + this.id);	
 			var idUpdate = idUpdate0.replace(/id="/g, 'id="' + this.id);
