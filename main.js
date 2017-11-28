1f9f31560764f2e55265353a29a5450701ce6e21
laflessn <laura.flessner@tnc.org>
2017-11-28 14:08:24 -0800

    dev to dev service

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function ( 	declare, PluginBase, ContentPane, dom, domStyle, domGeom, obj, conte
 			declare.safeMixin(this, frameworkParameters);
 			// Define object to access global variables from JSON object. Only add variables to varObject.json that are needed by Save and Share. 
 			this.obj = dojo.eval("[" + obj + "]")[0];	
-			this.url = "http://dev.services.coastalresilience.org:6080/arcgis/rest/services/North_Carolina/NC_CRS_New/MapServer";
+			this.url = "http://dev.services.coastalresilience.org:6080/arcgis/rest/services/North_Carolina/CRS/MapServer";
 			this.layerDefs = [];
 		},
 		// Called after initialize at plugin startup (why the tests for undefined). Also called after deactivate when user closes app by clicking X. 
